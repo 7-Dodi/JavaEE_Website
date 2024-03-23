8c91c3f9e1cb0553aeed772b4aaf1aa23518143e
Douglas 勝雄 <107079647+7-Dodi@users.noreply.github.com>
2024-03-23 11:44:13 -0300

    Update ConnectionManufature.java

--- src/main/java/br/com/JavaWeb/connection/ConnectionManufature.java
+++ src/main/java/br/com/JavaWeb/connection/ConnectionManufature.java
@@ -9,7 +9,7 @@ public class ConnectionManufature {
 	public Connection getConnection() throws ClassNotFoundException {
 		try {
 			Class.forName("org.postgresql.Driver");
-			return DriverManager.getConnection("jdbc:postgresql://localhost:5432/vaccineManager", "postgres", "dx=26/12");
+			return DriverManager.getConnection("jdbc:postgresql://localhost:5432/nomeDoBancoDeDados", "postgres", "suaSenha");
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
 		}
